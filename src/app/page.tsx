bdf06527cac08cbe27b2cb31b55d06389a67c712
Paulo Turino <paulo.turino01@gmail.com>
2023-09-08 09:09:04 -0300

    adicionando teste do aos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
+'use client'
+
+import { useEffect, useRef } from 'react';
 import { AiFillStar } from 'react-icons/ai'
+import { Typewriter, Cursor } from 'react-simple-typewriter';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 export default function Home() {
+
+  const root = useRef<HTMLElement>(null);
+  const bg = useRef<HTMLDivElement>(null);
+
+  const handleBg = (e: MouseEvent) => {
+    if (bg.current !== null){
+      bg.current.style.left = `${e.clientX}px`
+      bg.current.style.top = `${e.clientY}px`
+    }
+  }
+
+  useEffect(() => {
+    root.current?.addEventListener("mousemove", handleBg)
+  }, [])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-6xl flex gp-2 text-orange-500'><AiFillStar />Hello World<AiFillStar /></h1>
+    <main className="flex flex-col p-24 text-6xl text-orange-500">
+      <section className="flex min-h-screen">
+        <AiFillStar />
+        <h1 className='flex gp-2'><Typewriter words={['Hello, World!']} loop={3} cursor={true} cursorStyle=''/></h1>
+        <Cursor />
+        <AiFillStar />
+      </section>
+      
+      <section
+        className="flex flex-col items-center justify-center bg-gray-500"
+        data-aos="fade-down"
+        data-aos-duration="1000"
+      >
+        <h2>oi</h2>
+      </section>
+      <section
+        className="flex flex-col items-center justify-center"
+        data-aos="fade-right"
+        data-aos-duration="1000"
+      >
+        <h2 className='bg-gray-500'>aiaiaiaiai</h2>
+        <h2 className='bg-gray-500'>aiaiaiaiai</h2>
+        <h2 className='bg-gray-500'>aiaiaiaiai</h2>
+      </section>
     </main>
   )
 }
